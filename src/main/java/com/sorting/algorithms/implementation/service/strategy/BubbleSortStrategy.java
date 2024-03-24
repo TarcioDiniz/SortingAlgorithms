90b63ecb68854a162b425ea9a58dda79ab145a59
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-03-24 11:55:13 -0300

    feat: implementação BubbleSort

--- src/main/java/com/sorting/algorithms/implementation/service/strategy/BubbleSortStrategy.java
+++ src/main/java/com/sorting/algorithms/implementation/service/strategy/BubbleSortStrategy.java
@@ -6,6 +6,17 @@ public class BubbleSortStrategy implements AlgorithmsStrategy {
     @Override
     public int[] sorting(int[] array) {
 
-        return null;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j+1] = temp;
+
+                }
+            }
+        }
+
+        return array;
     }
 }
