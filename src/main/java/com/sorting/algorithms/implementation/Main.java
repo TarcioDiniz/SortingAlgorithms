0fbe823fc7085a60d9a7f14480014d13556cb127
Tarcio Diniz <tarciodiniz0@gmail.com>
2024-03-24 13:26:41 -0300

    feat: Insertion Sort

--- src/main/java/com/sorting/algorithms/implementation/Main.java
+++ src/main/java/com/sorting/algorithms/implementation/Main.java
@@ -17,7 +17,7 @@ public class Main {
         int[] integersArray = FileUtil.readIntegersFromFile(test_array_1M);
 
 
-        System.out.println(Arrays.toString(AlgorithmsApplication.sorting(AlgorithmsType.MERGE_SORT, arrayTest)));
+        System.out.println(Arrays.toString(AlgorithmsApplication.sorting(AlgorithmsType.INSERTION_SORT, arrayTest)));
 
     }
 }
--- src/main/java/com/sorting/algorithms/implementation/service/strategy/InsertionSortStrategy.java
+++ src/main/java/com/sorting/algorithms/implementation/service/strategy/InsertionSortStrategy.java
@@ -5,6 +5,16 @@ import com.sorting.algorithms.implementation.interfaces.AlgorithmsStrategy;
 public class InsertionSortStrategy implements AlgorithmsStrategy {
     @Override
     public int[] sorting(int[] array) {
-        return null;
+        for (int i = 1; i < array.length; ++i) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+        return array;
     }
 }
